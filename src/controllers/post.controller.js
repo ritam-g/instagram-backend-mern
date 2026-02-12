e0e630e3b81753a19c88ed3b71e3885be16210a2
ritam-g <ritammaty2006@gmail.com>
2026-02-12 22:19:03 +0530

    refactor: enhance post controller logic for improved readability and structure

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,20 +1,61 @@
 const postModel = require("../model/post.model");
 const ImageKit = require('@imagekit/nodejs');
-
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/user.model");
 const imagekit = new ImageKit({
-  publicKey: "xxx",
-  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
-  urlEndpoint: "https://ik.imagekit.io/xxx"
+    publicKey: "xxx",
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+    urlEndpoint: "https://ik.imagekit.io/xxx"
 });
 
 async function postController(req, res) {
+
+    //NOTE - //! now doing valid one  
+
+    /**
+     *      
+     * //* INFO  mkae proper folder structure for image kit   
+     * //* INFO  enough for the day    
+     */
+    //* INFO  create peroper post controler 
+    const { caption } = req.body
+
+    //* INFO  check the cokkie exiest or not  
+    const token = req.cookies.token;
+
+    if (!token) return res.status(401).json({ message: "user is unauthorize" })
+
+    //* INFO  if exieste check valid or not 
+    let decode = null
+    try {
+        decode = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(400).json({ messsage: err })
+    }
+
+    let user = await userModel.findOne({ _id: decode.id })
+    if (!user) return res.status(401).json({ message: "user is unauthorize" })
 
     const file = await imagekit.files.upload({
         file: req.file.buffer.toString("base64"),
         fileName: "Test"
     });
 
-    res.send(file);
+    //* INFO  then create post  
+
+    const post = await postModel.create({
+        caption,imgUrl:file.url,user:user._id
+    })
+
+    return res.status(201).json({
+        message:'post is created ',
+        post
+    })
+
+
+
+
+
 }
 
 
