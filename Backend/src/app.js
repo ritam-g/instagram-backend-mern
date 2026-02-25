499e037cc32b4d16ea4bf0527f118abd035f8d5e
ritam-g <ritammaty2006@gmail.com>
2026-02-25 19:05:15 +0530

    app.js3

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,23 +8,24 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static("./public"))
 // CORS
 const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
 ].filter(Boolean);
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true
-  })
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+            return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true
+    })
 );
 
 // Routes
@@ -36,16 +37,16 @@ app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
 
-// ✅ Production static serving
-if (process.env.NODE_ENV === "production") {
-  const publicPath = path.join(__dirname, "..", "public");
 
-  app.use(express.static(publicPath));
+app.use('*name',(req,res)=>{
+    //! IF THER IS ANY UNKNOW ROUTE SO IT WILL SHOW THSI INDX.HTML FILE ROUTE 
+    //! BUT ITS NOT GOOD OF DING FULL FILE OF THE YOUR SYSTEM 
+    //! FOR THAT WE HAVE  
+    console.log(__dirname);
+      //__dirname is the locaiton of till backend to src file 
+    res.sendFile(path.join(__dirname,'..','\\public\\index.html'))
+    
+})
 
-  // Express v5 wildcard fix
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
-  });
-}
 
 module.exports = app;
